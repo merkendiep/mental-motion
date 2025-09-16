4b75be411ac5e593f8998f1a957fb24e5b6e8aa3
Maurits@werk-laptop <maurits.erkendiep@a-insights.eu>
2025-09-16 19:51:59 +0200

    Chagne text for join page + some more minor improvements

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -1,109 +1,110 @@
-'use client'
+"use client";
 
-import React from 'react';
-import TransitionWithBorder from '@/src/components/TransitionWithBorder';
+import React from "react";
+import TransitionWithBorder from "@/src/components/TransitionWithBorder";
 
 const JoinUs = () => {
-    return (
-        <div className="flex flex-col bg-white pt-24 lg:pt-44">
-            <div className="hero-content flex-col mx-auto gap-8 lg:flex-row">
-                <img
-                    className="mask mask-square rounded-xl max-w-xs md:max-w-2xl"
-                    src="/images/training-presentation.jpeg"
-                />
+  return (
+    <div className="flex flex-col bg-white pt-24 lg:pt-44">
+      <div className="hero-content flex-col mx-auto gap-8 lg:flex-row">
+        <img
+          className="mask mask-square rounded-xl max-w-xs md:max-w-2xl"
+          src="/images/training-presentation.jpeg"
+        />
 
-                <div className="text-center lg:text-start">
-                    <h1 className="text-3xl font-black text-gray-700 uppercase mb-8 md:text-7xl">
-                        <span>Sluit je bij ons aan!</span>
-                    </h1>
+        <div className="text-center lg:text-start">
+          <h1 className="text-3xl font-black text-gray-700 uppercase mb-8 md:text-7xl">
+            <span>Sluit je bij ons aan!</span>
+          </h1>
 
-                    <p className={'mb-6'}>
-                        We willen de eigen regie van en studenten vergroten en hebben jouw daarbij nodig!
-                        MentalMotion is een plek waar we beweging generen.
-                        Wordt peersupporter of bedenk samen met medestudenten wat er beter kan in Utrecht en maak samen
-                        het verschil door beleidsverandering in Utrecht te bewerkstelligen.
-                        Zet samen met medestudenten een initiatief op en kom in actie.
-                        Mag gaan over elk thema waarvan jij denkt dat het te maken heeft met studentenwelzijn.
-                    </p>
-                </div>
-            </div>
-
-            <TransitionWithBorder colorFrom={'bg-white'} colorTo={'bg-[#58B095]'}/>
-
-            <div className={' max-w-full w-full bg-[#58B095]'}>
-                <div className="hero-content flex-col mx-auto text-white lg:gap-16 lg:flex-row-reverse">
-                    <img
-                        className="mask mask-heart max-w-xs md:max-w-lg"
-                        src="/images/peer-support-example-conversation.jpeg"
-                    />
-
-                    <div className="text-center lg:text-start">
-                        <h1 className="text-3xl font-black text-white uppercase md:text-7xl">
-                            <span>Wordt peersupporter!</span>
-                        </h1>
-
-                        <p className="py-6 lg:max-w-lg">
-                            Als peersupporter ben je op onze locatie aanwezig en sta je klaar voor laagdrempelige
-                            gesprekken met medestudenten.
-                            Je ondersteunt hen vanuit je eigen ervaringen als student en draagt bij aan hun welzijn.
-                        </p>
-
-                        <p className={'mb-6'}>
-                            Wij bieden trainingen en cursussen aan,
-                            zodat je precies weet hoe je een goed gesprek voert en
-                            waar je studenten naartoe kunt verwijzen. Je krijgt alle tools om zelfverzekerd peersupport
-                            te bieden! Je krijgt begeleiding en een vrijwilligersvergoeding!
-                        </p>
-
-                        <p className={'mb-6'}>Help mee aan het welzijn van de Utrechtse student!</p>
-                    </div>
-                </div>
-            </div>
-
-            <TransitionWithBorder colorFrom={'bg-[#58B095]'} colorTo={'bg-white'}/>
-
-            <div className="hero-content flex-col mx-auto lg:mt-16 lg:gap-16 lg:flex-row">
-                <img
-                    className="mask mask-square rounded-xl max-w-xs md:max-w-2xl"
-                    src="/images/mental-motion-outside-logo.jpeg"/>
-
-                <div className="text-center lg:text-start">
-                    <h1 className="text-3xl font-black text-gray-700 uppercase md:text-7xl">
-                        <span>Kom langs!</span>
-                    </h1>
-
-                    <p className="py-6 lg:max-w-lg">
-                        Wil jij je preventief inzetten voor het welzijn van studente?
-                        Kom langs, denk mee, zet zelf samen met ons een nieuw initiatief op.
-                    </p>
-
-                    <p>Wij horen graag wat jij denkt en hoe jij denkt dat de Utrechtse student het beste geholpen is.</p>
-
-                    <p>
-                        Wij helpen jou bij jouw idee. Wil je de gemeente spreken, de burgemeester of een wethouder.
-                        Heb jij een wetsvoorstel of iets over studentenwelzijn dat je graag zou willen dat
-                        Gemeente Utrecht moet veranderen?
-                    </p>
-
-                    <p>
-                        Laat je stem horen, kom bij ons langs en dan gaan we
-                        samen zorgen dat hier verandering in komt.
-                    </p>
-
-                    <p className={'mb-6'}>
-                        Wij bieden trainingen en cursussen aan,
-                        zodat je precies weet hoe je een goed gesprek voert en
-                        waar je studenten naartoe kunt verwijzen. Je krijgt alle tools om zelfverzekerd peersupport
-                        te bieden! Je krijgt begeleiding en een vrijwilligersvergoeding!
-                    </p>
-
-                    <p className={'mb-6'}>Help mee aan het welzijn van de Utrechtse student!</p>
-                </div>
-            </div>
-
-            <TransitionWithBorder colorFrom={'bg-white'} colorTo={'bg-gray-900'}/>
+          <p className={"mb-6"}>
+            MentalMotion is een plek waar we beweging generen, maar dat kunnen
+            we niet alleen. We willen de eigen regie van studenten en
+            jongvolwassenen vergroten. Ben jij student, jongvolwassene of jong
+            van geest, en wil jij je inzetten voor de mental mindshift die nodig
+            is? Wordt peer supporter, denk mee over wat er beter kan of
+            organiseer zelf een activiteit bij ons. Alles mag, zolang jij denkt
+            dat het te maken heeft met studentenwelzijn. Samen maken we het
+            verschil in heel Utrecht.
+          </p>
         </div>
-    );
+      </div>
+
+      <TransitionWithBorder colorFrom={"bg-white"} colorTo={"bg-[#58B095]"} />
+
+      <div className={" max-w-full w-full bg-[#58B095]"}>
+        <div className="hero-content flex-col mx-auto text-white lg:gap-16 lg:flex-row-reverse">
+          <img
+            className="mask mask-heart max-w-xs md:max-w-lg"
+            src="/images/peer-support-example-conversation.jpeg"
+          />
+
+          <div className="text-center lg:text-start">
+            <h1 className="text-3xl font-black text-white uppercase md:text-7xl">
+              <span>Wordt peersupporter!</span>
+            </h1>
+
+            <p className="py-6 lg:max-w-lg">
+              Als peer supporter ben je op onze locatie aanwezig en sta je klaar
+              voor laagdrempelige gesprekken met medestudenten. Je ondersteunt
+              hen vanuit je eigen ervaringen als student of jongvolwassene en
+              draagt bij aan hun welzijn. Wij bieden trainingen en cursussen
+              aan, zodat je precies weet hoe je een goed gesprek voert en waar
+              je studenten naartoe kunt verwijzen.
+            </p>
+
+            <p className={"mb-6"}>
+              Je krijgt alle tools om zelfverzekerd peer support te bieden! Ook
+              is er een keer per maand een vrijwilligersdag, en hebben we een
+              achterwacht van professionals waar je terecht kan als je extra
+              ondersteuning nodig hebt. Ook is er een vrijwilligersvergoeding,
+              en vooral heel veel gezelligheid!
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <TransitionWithBorder colorFrom={"bg-[#58B095]"} colorTo={"bg-white"} />
+
+      <div className="hero-content flex-col mx-auto lg:mt-16 lg:gap-16 lg:flex-row">
+        <img
+          className="mask mask-square rounded-xl max-w-xs md:max-w-2xl"
+          src="/images/mental-motion-outside-logo.jpeg"
+        />
+
+        <div className="text-center lg:text-start">
+          <h1 className="text-3xl font-black text-gray-700 uppercase md:text-7xl">
+            <span>Kom langs!</span>
+          </h1>
+
+          <p className="py-6 lg:max-w-lg">
+            Ben je benieuwd of MentalMotion iets voor jou is? Heb je een idee
+            maar weet je nog niet hoe dat werkelijkheid kan worden? Of je nou
+            zorgen hebt over een van je vrienden, je misschien peer supporter
+            wil worden of gewoon nieuwsgierig bent naar wie we zijn en wat we
+            doen, je bent altijd welkom om bij ons binnen te lopen! Check de{" "}
+            <a
+              href="/calendar"
+              className="text-[#58B095] underline hover:no-underline font-semibold"
+            >
+              agenda
+            </a>{" "}
+            voor alle aankomende activiteiten, of onze{" "}
+            <a
+              href="/contact"
+              className="text-[#58B095] underline hover:no-underline font-semibold"
+            >
+              openingstijden
+            </a>{" "}
+            zodat je niet voor een dichte deur hoeft te staan. Wij horen heel
+            graag van je!
+          </p>
+        </div>
+      </div>
+
+      <TransitionWithBorder colorFrom={"bg-white"} colorTo={"bg-gray-900"} />
+    </div>
+  );
 };
 
 export default JoinUs;
--- app/partners/page.tsx
+++ app/partners/page.tsx
@@ -1,119 +1,125 @@
-'use client'
+"use client";
 
-import React from 'react';
-import TransitionWithBorder from '@/src/components/TransitionWithBorder';
+import React from "react";
+import TransitionWithBorder from "@/src/components/TransitionWithBorder";
 
 const partners = [
-    {
-        name: 'Gemeente Utrecht',
-        logo: '/images/partners/GemeenteUtrecht.png',
-        description: '[Text over inzet van partner]',
-        website: '',
-    },
-    {
-        name: 'Hogeschool Utrecht',
-        logo: '/images/partners/HU.jpg',
-        description: '[Text over inzet van partner]',
-        website: '',
-    },
-    {
-        name: 'MBO Utrecht',
-        logo: '/images/partners/MBO-Utrecht.jpg',
-        description: '[Text over inzet van partner]',
-        website: '',
-    },
-    {
-        name: 'Nimento Utrecht',
-        logo: '/images/partners/NimentoUtrecht.png',
-        description: '[Text over inzet van partner]',
-        website: '',
-    },
-    {
-        name: 'Grafisch Lyceum Utrecht',
-        logo: '/images/partners/GLU.png',
-        description: '[Text over inzet van partner]',
-        website: '',
-    },
-    {
-        name: 'Dock.nl',
-        logo: '/images/partners/dockNL.jpg',
-        description: '[Text over inzet van partner]',
-        website: '',
-    },
-    {
-        name: 'Frisse Gedachtes',
-        logo: '/images/partners/Frisse-gedachtes.png',
-        description: '[Text over inzet van partner]',
-        website: '',
-    },
-    {
-        name: 'Geluks BV',
-        logo: '/images/partners/geluks-bv.jpg',
-        description: '[Text over inzet van partner]',
-        website: '',
-    },
-    {
-        name: 'HKU',
-        logo: '/images/partners/HKU.jpg',
-        description: '[Text over inzet van partner]',
-        website: '',
-    },
-    {
-        name: 'IDIUS',
-        logo: '/images/partners/IDIUS.png',
-        description: '[Text over inzet van partner]',
-        website: '',
-    },
-    {
-        name: 'JOU jongerenwerk Utrech',
-        logo: '/images/partners/JouJongerenWerk.png',
-        description: '[Text over inzet van partner]',
-        website: '',
-    },
-    {
-        name: 'Power Peers',
-        logo: '/images/partners/PowerPeers.png',
-        description: '[Text over inzet van partner]',
-        website: '',
-    },
+  {
+    name: "Dock.nl",
+    logo: "/images/partners/dockNL.jpg",
+    description: "[Text over inzet van partner]",
+    website: "",
+  },
+  {
+    name: "Frisse Gedachtes",
+    logo: "/images/partners/Frisse-gedachtes.png",
+    description: "[Text over inzet van partner]",
+    website: "",
+  },
+  {
+    name: "Geluks BV",
+    logo: "/images/partners/geluks-bv.jpg",
+    description: "[Text over inzet van partner]",
+    website: "",
+  },
+  {
+    name: "Gemeente Utrecht",
+    logo: "/images/partners/GemeenteUtrecht.png",
+    description: "[Text over inzet van partner]",
+    website: "",
+  },
+  {
+    name: "Grafisch Lyceum Utrecht",
+    logo: "/images/partners/GLU.png",
+    description: "[Text over inzet van partner]",
+    website: "",
+  },
+  {
+    name: "HKU",
+    logo: "/images/partners/HKU.jpg",
+    description: "[Text over inzet van partner]",
+    website: "",
+  },
+  {
+    name: "Hogeschool Utrecht",
+    logo: "/images/partners/HU.jpg",
+    description: "[Text over inzet van partner]",
+    website: "",
+  },
+  {
+    name: "IDIUS",
+    logo: "/images/partners/IDIUS.png",
+    description: "[Text over inzet van partner]",
+    website: "",
+  },
+  {
+    name: "JOU jongerenwerk Utrech",
+    logo: "/images/partners/JouJongerenWerk.png",
+    description: "[Text over inzet van partner]",
+    website: "",
+  },
+  {
+    name: "MBO Utrecht",
+    logo: "/images/partners/MBO-Utrecht.jpg",
+    description: "[Text over inzet van partner]",
+    website: "",
+  },
+  {
+    name: "Nimento Utrecht",
+    logo: "/images/partners/NimentoUtrecht.png",
+    description: "[Text over inzet van partner]",
+    website: "",
+  },
+  {
+    name: "Power Peers",
+    logo: "/images/partners/PowerPeers.png",
+    description: "[Text over inzet van partner]",
+    website: "",
+  },
 ];
 
 const PartnersPage = () => {
-    return (
-        <div className="flex flex-col bg-white pt-24 lg:pt-44">
-            <div className={'max-w-7xl mb-16 mx-auto px-2 md:px-0'}>
-                <div className="hero-content flex-col mx-auto gap-8 lg:flex-row">
-                    <div className="text-center lg:text-center">
-                        <h1 className="text-3xl font-black text-gray-700 mb-8 uppercase md:text-6xl">
-                            <span>Onze Partners</span>
-                        </h1>
+  return (
+    <div className="flex flex-col bg-white pt-24 lg:pt-44">
+      <div className={"max-w-7xl mb-16 mx-auto px-2 md:px-0"}>
+        <div className="hero-content flex-col mx-auto gap-8 lg:flex-row">
+          <div className="text-center lg:text-center">
+            <h1 className="text-3xl font-black text-gray-700 mb-8 uppercase md:text-6xl">
+              <span>Onze Partners</span>
+            </h1>
 
-                        <p className="text-lg font-medium text-gray-600 mb-8 md:text-xl max-w-2xl mx-auto">
-                            Samen met onze partners maken we Utrecht creatiever, gezonder en gelukkiger. 
-                            Hun energie, ideeën en betrokkenheid zorgen ervoor dat we écht impact maken in de stad.<br /><br />
-                            Of je nu een organisatie, ondernemer of enthousiasteling bent: sluit je aan en bouw mee aan een Utrecht vol beweging, verbinding en inspiratie!
-                        </p>
-                    </div>
-                </div>
-
-                <div className={'flex justify-center flex-row flex-wrap gap-8'}>
-                    {
-                        partners.map((partner, index) => {
-                            return <div key={index} className="rounded-lg flex justify-center items-center bg-white w-88 py-4 shadow-sm lg:w-96">
-                                <figure>
-                                    <img
-                                        src={partner.logo}
-                                        alt={partner.name + ' logo'}/>
-                                </figure>
-                            </div>
-                        })
-                    }
-                </div>
-            </div>
-
-            <TransitionWithBorder colorFrom={'bg-white'} colorTo={'bg-gray-900'}/>
+            <p className="text-lg font-medium text-gray-600 mb-8 md:text-xl max-w-2xl mx-auto">
+              Samen met onze partners maken we Utrecht creatiever, gezonder en
+              gelukkiger. Hun energie, ideeën en betrokkenheid zorgen ervoor dat
+              we écht impact maken in de stad.
+              <br />
+              <br />
+              Of je nu een organisatie, ondernemer of enthousiasteling bent:
+              sluit je aan en bouw mee aan een Utrecht vol beweging, verbinding
+              en inspiratie!
+            </p>
+          </div>
         </div>
-    );
+
+        <div className={"flex justify-center flex-row flex-wrap gap-8"}>
+          {partners.map((partner, index) => {
+            return (
+              <div
+                key={index}
+                className="rounded-lg flex justify-center items-center bg-white w-88 py-4 shadow-sm lg:w-96"
+              >
+                <figure>
+                  <img src={partner.logo} alt={partner.name + " logo"} />
+                </figure>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <TransitionWithBorder colorFrom={"bg-white"} colorTo={"bg-gray-900"} />
+    </div>
+  );
 };
 
 export default PartnersPage;
