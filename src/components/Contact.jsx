1c92ef2f6d861aefd5df8cf024e07579ec527c59
Maurits@werk-laptop <maurits.erkendiep@a-insights.eu>
2025-09-22 20:37:38 +0200

    Make sure socials link actually work

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -95,27 +95,7 @@ const Contact = () => {
               <div className="flex gap-3">
                 <a
                   className="btn btn-circle bg-base-100 hover:bg-base-200 group border border-base-300"
-                  href="https://www.youtube.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  aria-label="youtube"
-                >
-                  <svg
-                    className="h-6 w-6 fill-current group-hover:text-red-500 transition-colors"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                </a>
-
-                <a
-                  className="btn btn-circle bg-base-100 hover:bg-base-200 group border border-base-300"
-                  href="https://www.instagram.com"
+                  href="https://www.instagram.com/mentalmotion_utrecht/"
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label="instagram"
@@ -135,7 +115,7 @@ const Contact = () => {
 
                 <a
                   className="btn btn-circle bg-base-100 hover:bg-base-200 group border border-base-300"
-                  href="https://www.linkedin.com"
+                  href="https://www.linkedin.com/company/mentalmotion-spsc/"
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label="linkedin"
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -87,23 +87,7 @@ const Footer = () => {
           </h6>
           <div className="flex gap-4 mb-4">
             <a
-              href="https://youtube.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="YouTube"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="28"
-                height="28"
-                viewBox="0 0 24 24"
-                className="fill-current hover:text-red-500 transition-colors"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
-            </a>
-            <a
-              href="https://instagram.com"
+              href="https://www.instagram.com/mentalmotion_utrecht/"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="Instagram"
@@ -119,7 +103,7 @@ const Footer = () => {
               </svg>
             </a>
             <a
-              href="https://linkedin.com"
+              href="https://www.linkedin.com/company/mentalmotion-spsc/"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="LinkedIn"
